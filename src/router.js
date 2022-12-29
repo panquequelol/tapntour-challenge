aa9318140f6233c9768fe40180272df83d1cbd71
panquequelol <rene@outlook.my>
2022-12-29 17:59:31 -0300

    feat: add tour detail page using path params

--- src/router.js
+++ src/router.js
@@ -1,41 +1,48 @@
 import { createWebHistory, createRouter } from "vue-router";
 
 import HomeLayout from "@/layouts/HomeLayout.vue";
-import TourView   from "@/menu/tour/TourView.vue";
-import GuideView  from "@/menu/guide/GuideView.vue";
+import TourView from "@/menu/tour/TourView.vue";
+import GuideView from "@/menu/guide/GuideView.vue";
 import SignUpView from "@/menu/profile/SignUpView.vue";
 
 import TourDetail from "@/menu/tour/TourDetail.vue";
 
 const routes = [
-    {
-      path: "/",
-      name: "HomeLayout",
-      component: HomeLayout,
-    },
-    {
-      path: "/tours",
-      name: "TourView",
-      component: TourView,
-    },
-    {
-      path: "/guides",
-      name: "GuideView",
-      component: GuideView,
-    },
-    {
-      path: "/profile",
-      name: "SignUpView",
-      component: SignUpView,
-    },
-    {
-      path: "/TourDetail",
-      name: "TourDetail",
-      component: TourDetail,
-    }
-  ];
-  
-  const router = createRouter({
-    history: createWebHistory(), routes });
-  
+  {
+    path: "/",
+    name: "HomeLayout",
+    component: HomeLayout,
+  },
+  {
+    path: "/tours",
+    name: "TourView",
+    component: TourView,
+  },
+  {
+    path: "/tours/:id",
+    name: "TourDetail",
+    component: TourDetail,
+  },
+  {
+    path: "/guides",
+    name: "GuideView",
+    component: GuideView,
+  },
+  {
+    path: "/profile",
+    name: "SignUpView",
+    component: SignUpView,
+  },
+  // {
+  //   path: "/TourDetail",
+  //   name: "TourDetail",
+  //   component: TourDetail,
+  // }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
 export default router;
