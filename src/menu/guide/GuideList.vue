fdb9ddab075d0f0cf5eea6b7c9068ed502248498
panquequelol <rene@outlook.my>
2022-12-29 15:00:44 -0300

    feat: setup guides store and fetch guides

--- src/menu/guide/GuideList.vue
+++ src/menu/guide/GuideList.vue
@@ -1,30 +1,44 @@
 <template>
   <div class="divide-y divide-slate-100 pt-5 cursor-pointer">
     <NavView>
-      <NavItem href="" v-on:click:active=true  isActive>New Guide</NavItem>
-      <NavItem href="" v-on:click:active=true >Top Guide</NavItem>
+      <NavItem href="" v-on:click:active=true isActive>New Guide</NavItem>
+      <NavItem href="" v-on:click:active=true>Top Guide</NavItem>
     </NavView>
     <ListView>
-  
+      <p v-if="loading">Loading tours...</p>
+      <p v-if="error">{{ error.message }}</p>
+      <GuideItem v-if="guides" v-for="(item, index) in tours" :key="index" :item="item" />
     </ListView>
   </div>
 </template>
 
-<script> 
-  import { defineComponent,computed } from "vue"
-  import NavView       from '@/components/NavView.vue'
-  import NavItem       from '@/components/NavItem.vue'
-  import ListView      from '@/components/ListView.vue'
-  import GuideItem     from '@/menu/guide/GuideItem.vue'
+<script>
+import { defineComponent, computed } from "vue"
+import { storeToRefs } from 'pinia'
+import { useGuideStore } from '@/stores/guide'
+import NavView from '@/components/NavView.vue'
+import NavItem from '@/components/NavItem.vue'
+import ListView from '@/components/ListView.vue'
+import GuideItem from '@/menu/guide/GuideItem.vue'
 
-  export default defineComponent({
-    components: {
+export default defineComponent({
+  components: {
     NavView,
     NavItem,
     ListView,
     GuideItem
-},
-    setup() {
+  },
+  setup() {
+    const guideStore = useGuideStore()
+
+    guideStore.fetchGuides()
+
+    // making the store reactive
+    const { guides, loading, error } = storeToRefs(guideStore)
+
+    return {
+      guides, loading, error
+    }
   },
   methods: {
 
--- src/stores/guide.js
+++ src/stores/guide.js
@@ -1,4 +1,28 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-const guide_endpoint = 'https://tapntour-wk-db.glyfo.workers.dev/api/fetchAllGuides'
+const guide_endpoint = "https://tapntour-wk-db.glyfo.workers.dev/api/guides";
 
+export const useGuideStore = defineStore({
+  id: "guide",
+  store: () => ({
+    guides: [],
+    guide: null,
+    loading: false,
+    error: null,
+  }),
+  actions: {
+    async fetchGuides() {
+      this.guides = [];
+      this.loading = true;
+      try {
+        const response = await fetch(guide_endpoint, { method: "POST" });
+        const data = await response.json();
+        this.guides = [...this.tours, ...data];
+      } catch (error) {
+        this.error = error;
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+});
