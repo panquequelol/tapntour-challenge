6ced3fda87592c7c95e73f09c3813745270cf8f8
panquequelol <rene@outlook.my>
2022-12-29 21:44:06 -0300

    chore: pass props to description and feature components

--- src/components/FeatureItem.vue
+++ src/components/FeatureItem.vue
@@ -7,21 +7,21 @@
             <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
           </svg>
-          walk
+          {{mode}}
         </div>
         <!-- Stop -->   
         <div class="flex-1 inline-flex items-center" >     
           <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mx-1" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5" />
           </svg>
-          2 stop
+          {{stops}} stops
         </div>   
         <!-- Time -->
         <div class="flex-1 inline-flex items-center" >                 
           <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mx-1" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
              <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
-          6-8 hrs
+          {{duration}} minutes
         </div>
         <!-- Language -->
         <div class="flex-1 inline-flex items-center" >                 
@@ -37,12 +37,6 @@
 <script>
 export default {
   name: "FeactureItem",
-  data() {
-    return {
-      type: "",
-    };
-  },
-  methods: {
-  },
+  props: ['mode', 'duration', 'stops']
 };
 </script>
